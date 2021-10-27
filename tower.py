16dd46feacc36c8274b2d50d3fd75a80e4df59e0
Dawson Theroux <dawsontheroux@gmail.com>
2021-10-27 03:22:44 +0100

    Binary Image Rendered

--- tower.py
+++ tower.py
@@ -1,11 +1,13 @@
+
+
 import cv2
 
 cam = cv2.VideoCapture(0)
 ret, image = cam.read()
 
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-image = cv2.blur(image, (3,3))
-ret, image = cv2.threshold(image, 170, 255, cv2.THRESH_BINARY)
+image = cv2.blur(image, (2,2))
+ret, image = cv2.threshold(image, 50, 255, cv2.THRESH_TOZERO)
 
 print("Printing Image")
 cv2.imwrite('../testimage.jpg', image)
