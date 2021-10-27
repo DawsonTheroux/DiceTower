885db0b5f5b244ce69835fa3d2ec1bedf7d1acc3
Dawson_Theroux <dawson.theroux@corel.com>
2021-10-26 21:46:48 -0400

    Added a threshold

--- tower.py
+++ tower.py
@@ -1,10 +1,13 @@
 import cv2
 
 cam = cv2.VideoCapture(0)
-
 ret, image = cam.read()
 
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.blur(image, (3,3))
+ret, image = cv2.threshold(image, 170, 255, cv2.THRESH_BINARY)
+
 print("Printing Image")
-cv2.imwrite('/home/pi/testimage.jpg', image)
+cv2.imwrite('../testimage.jpg', image)
 cam.release()
 cv2.destroyAllWindows()
