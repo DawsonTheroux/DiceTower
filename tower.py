98d3511738abfe467a3d48b73f9b63a4fa284adb
Dawson Theroux <dawsontheroux@gmail.com>
2021-11-02 01:08:43 +0000

    Progress

--- tower.py
+++ tower.py
@@ -4,22 +4,25 @@ import math
 
 cam = cv2.VideoCapture(0)
 	
-def analyzeImage():
+def analyzeImage(filename):
 	ret, image = cam.read()
+	print(ret)
 	print("x: " + str(image.shape[0]) + "y: " + str(image.shape[1]))
 
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+	cv2.imwrite("../Gray" + filename + ".jpg",image)
 	image = cv2.blur(image, (3,3))	#Blur the imaget to help with binary detection.
-
+	cv2.imwrite("../Blur" + filename + ".jpg",image)
 	ret, image = cv2.threshold(image, 50, 255, cv2.THRESH_TOZERO) #Turn into binary image.
-
+	cv2.imwrite("../Threshold" + filename + ".jpg",image)
 	image = cv2.Canny(image, 80, 230) #Create contour lines of the binary image.
-
+	cv2.imwrite("../Canny" + filename + ".jpg",image)
 	dummy,contours, hierarchy = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+	
 	#Rantagles detected using the contours of the algorithm above.
 	dice_rects = []
+	i = 0
+	print("Contours:" + str(len(contours)))
 
 	for contour in contours:
 		rect = cv2.minAreaRect(contour)
@@ -38,24 +41,26 @@ def analyzeImage():
 					process = False
 					break
 			if process:
+				i += 1
 				dice_rects.append(rect)
-			
 				box = cv2.boxPoints(rect)
 				box = np.int0(box)
-				image = cv2.drawContours(image, [box], 0, (0,0,255),1)
-	
+				image = cv2.drawContours(image, [box], 0, (0,0,255),3)
+				cv2.imwrite("../Contour" + str(i) + filename + ".jpg",image)
 	print("Number Dice:" + str(len(dice_rects)))
 
-	image = cv2.putText(image, "Dice: " + str(len(dice_rects)), (10,10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv2.LINE_AA)
-
+	image = cv2.putText(image, "Dice: " + str(len(dice_rects)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv2.LINE_AA)
+	cv2.imwrite("../Final" + filename + ".jpg", image)
+filename = "default.jpg"
 while(True):
 	key = raw_input("Give a command: ")
 	if key == "q" or key  == "Q":
 		break
 	else:
-		analyzeImage()
+		analyzeImage(key)
 		print("Done Analyzing")
+
 print("Printing Image")
-cv2.imwrite('../testimage.jpg', image)
+#cv2.imwrite('../testimage.jpg', image)
 cam.release()
 cv2.destroyAllWindows()
